0bcd2d4e19589f1fc9f085e3a9c3ff24b7f7d378
wenyu <wenyutech@foxmail.com>
2020-07-27 16:43:04 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# http
-http get and post methods
+
+##### 经常在项目里面会用上GET和POST请求方法，使用封装好的方法会方便很多。
+
+## 一、安装
+
+```shell
+composer require fishtail/http
+```
+
+## 二、使用
+
+###  1. 导入
+```php
+use Fishtail\Http;
+```
+###  2. GET请求
+```php
+$res = Http::get('https://www.baidu.com');
+```
+###  3. POST请求
+```php
+$url = 'http://example.com';
+$header = [
+	'Accept: */*',
+	'Content-Type: text/plain;charset=UTF-8'
+];
+$cookie = 'token=2f2568ae-75f0-4ba9-98d4-8c139dad7f64';
+$postData = [
+	'page' => 1,
+	'pageSize' => 20
+];
+$res = Http::post($url, $postData, $cookie, $header);
+```
+
+## 三、参数解释
+
+###  1. GET方法
+| 参数  | 类型  |  描述 |
+| :------------ | :------------ | :------------ |
+| url | string  |  请求的url |
+| cookie |  string | 请求cookie  |
+| header | array  | 请求头数据  |
+| returnCookie | bool  | 是否返回cookie  |
+
+###  1. POST方法
+| 参数  | 类型  |  描述 |
+| :------------ | :------------ | :------------ |
+| url | string  |  请求的url |
+| data |  string\|array | post数据  |
+| cookie |  string | 请求cookie  |
+| header | array  | 请求头数据  |
+| returnCookie | bool  | 是否返回cookie  |
--- src/Fishtail/Http.php
+++ src/Fishtail/Http.php
@@ -8,23 +8,28 @@ namespace Fishtail;
  */
 class Http
 {
+    // User-Agent
     static $userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36';
+    // 请求超时时间
+    static $timeout = 30;
     /**
      * GET请求方法
      * @param string $url 请求的url
      * @param string $cookie 请求cookie
      * @param array $header 请求头数据
-     * @param int $returnCookie 是否返回cookie
-     * @return string|array $returnCookie==0时，返回请求返回内容；$returnCookie==1时，返回请求返回内容content和cookie
+     * @param bool $returnCookie 是否返回cookie
+     * @return string|array $returnCookie为假时，返回body；$returnCookie为真时，返回包含body和cookie的数组
      */
-    static function get($url, $cookie = '', $header = [], $returnCookie = 0)
+    static function get($url, $cookie = '', $header = [], $returnCookie = false)
     {
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($curl, CURLOPT_AUTOREFERER, 1);
-        curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($curl, CURLOPT_AUTOREFERER, true);
+        curl_setopt($curl, CURLOPT_TIMEOUT, self::$timeout);
         curl_setopt($curl, CURLOPT_USERAGENT, self::$userAgent);
         if ($header) {
             curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
@@ -33,8 +38,6 @@ class Http
             curl_setopt($curl, CURLOPT_COOKIE, $cookie);
         }
         curl_setopt($curl, CURLOPT_HEADER, $returnCookie);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
         $res = curl_exec($curl);
         if (curl_errno($curl)) { //请求出错
             return curl_error($curl);
@@ -42,13 +45,13 @@ class Http
         curl_close($curl);
         if ($returnCookie) {
             list($header, $body) = explode("\r\n\r\n", $res, 2);
-            preg_match_all("/Set\-Cookie:([^;]*);/i", $header, $matches); //匹配相应cookie
+            preg_match_all("/Set\-Cookie:([^;]*);/i", $header, $matches); //匹配cookie
             $cookie = '';
             foreach ($matches[1] as $value) {
                 $cookie = $value . ';' . $cookie;
             }
             $info['cookie']  = preg_replace('# #', '', $cookie);
-            $info['content'] = $body;
+            $info['body'] = $body;
             return $info;
         } else {
             return $res;
@@ -61,21 +64,23 @@ class Http
      * @param string|array $data post数据
      * @param string $cookie 请求cookie
      * @param array $header 请求头数据
-     * @param string $returnCookie 是否返回cookie
-     * @return string|array $returnCookie==0时，返回请求返回内容；$returnCookie==1时，返回请求返回内容content和cookie
+     * @param bool $returnCookie 是否返回cookie
+     * @return string|array $returnCookie为假时，返回body；$returnCookie为真时，返回包含body和cookie的数组
      */
-    static function post($url, $data, $cookie = '', $header = [], $returnCookie = 0)
+    static function post($url, $data, $cookie = '', $header = [], $returnCookie = false)
     {
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_POST, true);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($curl, CURLOPT_AUTOREFERER, 1);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($curl, CURLOPT_AUTOREFERER, true);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($curl, CURLOPT_USERAGENT, self::$userAgent);
-        curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+        curl_setopt($curl, CURLOPT_TIMEOUT, self::$timeout);
         if ($header) {
             curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
         }
@@ -83,8 +88,6 @@ class Http
             curl_setopt($curl, CURLOPT_COOKIE, $cookie);
         }
         curl_setopt($curl, CURLOPT_HEADER, $returnCookie);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
         $res = curl_exec($curl);
         if (curl_errno($curl)) { //请求出错
             return curl_error($curl);
@@ -92,13 +95,13 @@ class Http
         curl_close($curl);
         if ($returnCookie) {
             list($header, $body) = explode("\r\n\r\n", $res, 2);
-            preg_match_all("/Set\-Cookie:([^;]*);/i", $header, $matches); //匹配相应cookie
+            preg_match_all("/Set\-Cookie:([^;]*);/i", $header, $matches); //匹配cookie
             $cookie = '';
             foreach ($matches[1] as $value) {
                 $cookie = $value . ';' . $cookie;
             }
             $info['cookie']  = preg_replace('# #', '', $cookie);
-            $info['content'] = $body;
+            $info['body'] = $body;
             return $info;
         } else {
             return $res;
